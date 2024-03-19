be7fb6ae2d8424c3bc4663c1693c344a84699343
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-03-19 10:01:51 -0300

    add product details

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,7 @@ export * from './ui/not-found/PageNotFound';
 export * from './ui/sidebar/Sidebar';
 export * from './ui/title/Title';
 export * from './ui/top-menu/TopMenu';
+
+export * from './product/size-selector/SizeSelector';
+export * from './product/quantity-selector/QuantitySelector';
+export * from './product/slideshow/ProductSlideshow';
